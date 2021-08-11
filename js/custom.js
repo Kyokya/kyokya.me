da292966d9a5faa31be20a0f26aa3357b895b816
Kyokya <81727012+Kyokya@users.noreply.github.com>
2021-08-11 16:07:21 +0800

    Delete js directory

--- js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
-	$(window).resize(function(){
-        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
-    });
-
-});
